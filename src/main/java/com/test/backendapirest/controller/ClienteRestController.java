cd4986c537508055e8c722f7b034a6c54dca2def
Albin18 <albianjose0424@gmail.com>
2024-03-21 19:49:32 -0300

    api-clientes

--- src/main/java/com/test/backendapirest/controller/ClienteRestController.java
+++ src/main/java/com/test/backendapirest/controller/ClienteRestController.java
@@ -3,10 +3,8 @@ package com.test.backendapirest.controller;
 import com.test.backendapirest.model.entity.Cliente;
 import com.test.backendapirest.model.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,35 @@ private IClienteService clienteService;
 @GetMapping("/clientes")
     public List<Cliente> index(){
         return clienteService.findAll();
+    }
+
+    @GetMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Cliente show(@PathVariable Long id){
+    return clienteService.findById(id);
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+    return clienteService.save(cliente);
+    }
+
+    @PutMapping("/clientes/{id}")
+public Cliente update(@RequestBody Cliente cliente, @PathVariable long id){
+    Cliente clienteActual = clienteService.findById(id);
+
+    clienteActual.setApellido(cliente.getApellido());
+    clienteActual.setNombre(cliente.getNombre());
+    clienteActual.setEmail(cliente.getEmail());
+
+    return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+    clienteService.delete(id);
     }
 
 }
