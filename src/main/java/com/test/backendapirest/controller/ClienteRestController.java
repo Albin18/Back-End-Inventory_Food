e1b0768e0520de5e4c89b7b624b3be420be04c5d
Albin18 <albianjose0424@gmail.com>
2024-04-01 18:39:34 -0300

    excepciones

--- src/main/java/com/test/backendapirest/controller/ClienteRestController.java
+++ src/main/java/com/test/backendapirest/controller/ClienteRestController.java
@@ -3,10 +3,15 @@ package com.test.backendapirest.controller;
 import com.test.backendapirest.model.entity.Cliente;
 import com.test.backendapirest.model.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
@@ -21,9 +26,25 @@ private IClienteService clienteService;
 
     @GetMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Cliente show(@PathVariable Long id){
-    return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id){
+
+    Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+    try{
+        cliente = clienteService.findById(id);
+    } catch(DataAccessException e){
+        response.put("mensaje", "Error al realizar la consulta en la base de datos");
+        response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    if(cliente == null){
+        response.put("mensaje", "El cliente ID: ".concat(id.toString()).concat(" No existe en la base de datos!"));
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+    }
+
+
 
     @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
